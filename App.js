98d096f244a1db9591d1c569566f280a25738e05
J-Hannaway <jhannaway@moderndemocracy.com>
2020-04-22 09:10:33 +0100

    Refactor components to be functional

--- App.js
+++ App.js
@@ -3,38 +3,37 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import BrowserHandler from "./components/BrowserHandler";
-import { SafeAreaView, Platform, StatusBar, View } from 'react-native'
+import { SafeAreaView, StatusBar } from 'react-native'
 
 const Stack = createStackNavigator();
 
-class App extends Component {
+const App = () => {
+  return (
+    <>
+      <StatusBar
+        backgroundColor="#243a47"
+        barStyle="light-content"
+      />
+      <SafeAreaView
+        style={{ backgroundColor: '#243a47' }}
+      />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Browser">
 
-  componentDidMount(){
-    StatusBar.setBarStyle('light-content', true);
-  }
-
-  render() {
-    return (
-      <>
-        <SafeAreaView style={{ flex: 0, backgroundColor: '#243a47' }} />
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{ headerShown: false }}
-            initialRouteName="Browser">
-
-            <Stack.Screen
-              name="Browser"
-              component={BrowserHandler}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </>
-    );
-  }
+          <Stack.Screen
+            name="Browser"
+            component={BrowserHandler}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 };
 
 export default App;
--- components/BrowserHandler.js
+++ components/BrowserHandler.js
@@ -1,22 +1,13 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import { WebView } from 'react-native-webview';
-import { SafeAreaView, Platform } from 'react-native';
 
-class BrowserHandler extends Component {
-
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <WebView
-                ref={webView => (this.webView = webView)}
-                source={{ uri: 'https://www.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx' }}
-            />
-        );
-    }
-}
+const BrowserHandler = () => {
+    return (
+        <WebView
+            source={{ uri: 'https://www.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx' }}
+        />
+    );
+};
 
 export default BrowserHandler
