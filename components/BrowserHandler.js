95849394440801fb0c41e4459b5172ac2d23ceba
J-Hannaway <jhannaway@moderndemocracy.com>
2020-04-22 15:30:34 +0100

    Altered webview url depending on environment

--- components/BrowserHandler.js
+++ components/BrowserHandler.js
@@ -1,11 +1,20 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { WebView } from 'react-native-webview';
 
-const BrowserHandler = () => {
+const BrowserHandler = (props) => {
+
+    const [baseURL, setURl] = useState("https://www.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx");
+
+    useEffect(() => {
+        if (global.__DEV__) {
+            setURl("https://staging.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx")
+        }
+    });
+
     return (
         <WebView
-            source={{ uri: 'https://www.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx' }}
+            source={{ uri: baseURL }}
         />
     );
 };
