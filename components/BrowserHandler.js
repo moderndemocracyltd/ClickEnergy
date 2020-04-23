a23ae6d5f4b35f28fdfa794cbacf1f2a4c773169
J-Hannaway <jhannaway@moderndemocracy.com>
2020-04-23 15:11:16 +0100

    Added support for iPad and ext url handler

--- components/BrowserHandler.js
+++ components/BrowserHandler.js
@@ -1,22 +1,42 @@
 
 import React, { useEffect, useState } from 'react';
 import { WebView } from 'react-native-webview';
+import { Linking } from 'react-native';
 
 const BrowserHandler = (props) => {
+
+    webView = null;
 
     const [baseURL, setbaseURl] = useState("");
 
     useEffect(() => {
         if (global.__DEV__) {
-            setbaseURl("https://staging.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx");
+            setbaseURl("https://staging.clickenergyni.com");
         } else {
-            setbaseURl("https://www.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx");
+            setbaseURl("https://www.clickenergyni.com");
         }
-    },[]);
+    }, []);
+
+    const handleNavigationChange = async newNavState => {
+        const { url } = newNavState;
+
+        if (!url.includes(baseURL)) {
+            this.webView.stopLoading();
+
+            const supported = await Linking.canOpenURL(url);
+            if (supported) {
+                await Linking.openURL(url);
+            } else {
+                alert(`Can't open link: ${url}`);
+            }
+        }
+    }
 
     return (
         <WebView
-            source={{ uri: baseURL }}
+            ref={ref => this.webView = ref}
+            source={{ uri: `${baseURL}/?returnurl=%2fDashboard%2fSummary.aspx` }}
+            onNavigationStateChange={handleNavigationChange}
         />
     );
 };
