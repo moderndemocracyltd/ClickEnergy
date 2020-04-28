6e58271afcf9e02b1a604ce02e3c9e2ce64fc79e
Ben Donaghey <bdonaghey@MDL0108.local>
2020-04-28 11:54:20 +0100

    WIP

--- components/BrowserHandler.js
+++ components/BrowserHandler.js
@@ -1,22 +1,35 @@
 
 import React, { useEffect, useState } from 'react';
+import { BackHandler } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const BrowserHandler = (props) => {
 
     const [baseURL, setbaseURl] = useState("");
+    const WEBVIEW_REF = React.createRef();
+
+    handleBackButtonClick = () => {
+        WEBVIEW_REF.current.goBack(null);
+        return true;
+    }
 
     useEffect(() => {
+
         if (global.__DEV__) {
-            setbaseURl("https://staging.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx");
-        } else {
+        //     setbaseURl("https://staging.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx");
+        // } else {
             setbaseURl("https://www.clickenergyni.com/?returnurl=%2fDashboard%2fSummary.aspx");
         }
+
+        BackHandler.addEventListener("hardwareBackPress", handleBackButtonClick);
+        return () => BackHandler.removeEventListener("hardwareBackPress", handleBackButtonClick);
+
     },[]);
 
     return (
         <WebView
             source={{ uri: baseURL }}
+            ref={ WEBVIEW_REF }
         />
     );
 };
